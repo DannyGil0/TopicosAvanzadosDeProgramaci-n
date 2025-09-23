c9b0569c4d1abf399809ca27498866010576cea6
Daniel  Gil Gonzalez <178396@upslp.edu.mx>
2025-09-23 16:34:11 -0600

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>(); // Obtenemos el componente Rigidbody del objeto.
+        // playerRb.AddForce(Vector3.up * 1000f);
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (Input.GetKey(KeyCode.Space) && isOnGround){
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision){
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over");
+        }
+        isOnGround = true;
+    }
+}
