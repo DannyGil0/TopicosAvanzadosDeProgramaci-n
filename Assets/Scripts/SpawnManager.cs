1235031288e0980c548ae218100596ae45f8c487
Daniel GG <178396@upslp.edu.mx>
2025-09-24 16:18:21 -0600

    agregar archivos de Unity

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,10 +7,11 @@ public class SpawnManager : MonoBehaviour{
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2.0f;
     private float repeatRate = 2.0f;
- 
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start(){
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
  
     // Update is called once per frame
@@ -19,6 +20,9 @@ public class SpawnManager : MonoBehaviour{
     }
  
     void SpawnObstacle(){
+        if (playerControllerScript.gameOver == false)
+        {
         Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 }
